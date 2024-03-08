32872adf1235b9fdb21cbdab6a65bf2e6ff84787
Kikii95 <147034133+Kikii95@users.noreply.github.com>
2024-03-08 12:11:58 +0100

    BIG FINAL

--- Scripts/ScrollingBackground.cs
+++ Scripts/ScrollingBackground.cs
@@ -11,6 +11,7 @@ public class ScrollingBackground : MonoBehaviour
 
     void Update()
     {
+        transform.localScale = new Vector3(Camera.main.orthographicSize * 4.5f, Camera.main.orthographicSize * 2.2f, 1);
         bgRenderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
     }
 }
